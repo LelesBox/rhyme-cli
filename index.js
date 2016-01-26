514c12b3d7192fb27222e3aed786c08fb1445bc2
LelesBox <li_xiaoliang@foxmail.com>
2016-01-26 19:02:25 +0800

    update

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
 #!/usr/bin/env node
-//预计完成的功能 rhyme gen or rhyme duang
+//预计完成的功能 rhyme gen or rhyme duang --name test
+var templateUrl = "https://codeload.github.com/LelesBox/rhyme-template/zip/master"
 var argv = require('yargs').argv;
+var path = "rhyme-template"
+var name = process.argv[2];
+var shell = require("shelljs");
 
-
-console.log('hello ', argv.name);
+shell.exec("echo download template");
+shell.exec("wget " + templateUrl);
+shell.exec("unzip master && rm master")
+if (argv.name) {
+    path = argv.name
+}
+shell.exec("mv rhyme-template-master " + path)
+shell.exec("cd " + path + " && rm -rf .git && npm install")
+shell.exec("echo done!")
